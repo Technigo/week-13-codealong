16936a7258150f25cd5e608a65aa2ad8587b0b66
Damien <mail@damientimewell.com>
2019-12-18 18:00:45 +0100

    Codealong code!

--- App.js
+++ App.js
@@ -1,19 +1,70 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components/native'
+import { ActivityIndicator } from 'react-native'
+import { Movie } from './components/Movie'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const TOP_MOVIE_ID = 656516
+
+const Container = styled.View`
+  flex: 1;
+  background-color: black;
+  align-items: center;
+  justify-content: center;
+`
+
+const Button = styled.TouchableOpacity`
+  background: #80ff86;
+  position: absolute;
+  bottom: 40;
+  padding: 10px 20px;
+  border-radius: 20px;
+`
+
+const ButtonText = styled.Text`
+  color: #1f2e1f;
+  font-size: 18;
+`
+
+const fetchRandomMovie = async () => {
+  const movieId = Math.floor(Math.random() * TOP_MOVIE_ID) + 1
+  const res = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=6420add5c0a9b0e0b9462a92916c3187&language=en-US`)
+  const json = await res.json()
+
+  if (json.status_code === 34 || !json.poster_path || json.adult) {
+    return fetchRandomMovie()
+  } else {
+    return json
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  const [movie, setMovie] = useState()
+  const [loading, setLoading] = useState(true)
+
+  const fetchMovieData = () => {
+    setLoading(true)
+    fetchRandomMovie()
+      .then((movieData) => {
+        setMovie(movieData)
+        setLoading(false)
+      })
+  }
+
+  useEffect(() => {
+    fetchMovieData()
+  }, [])
+
+  return (
+    <Container>
+      {loading
+        ? <ActivityIndicator size="large" color="#ffffff" />
+        : <Movie movie={movie} />}
+
+      <Button onPress={fetchMovieData}>
+        <ButtonText>Fetch new movie</ButtonText>
+      </Button>
+    </Container>
+  )
+}
+
+export default App
